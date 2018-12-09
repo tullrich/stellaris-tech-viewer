9f5eb53dd842950b6857869b5144aee0c2130375
Tim Ullrich <trozen@gmail.com>
2018-12-09 16:27:26 -0600

    Improve layout of graph nodes a bit

--- src/app/technology-web/technology-web.component.ts
+++ src/app/technology-web/technology-web.component.ts
@@ -3,6 +3,14 @@ import { TechnologyLibraryService } from '../services/technology-library/technol
 import * as dagre from "dagre";
 import * as _ from "lodash";
 
+const LOW_WEIGHT_TECHS = {
+  "tech_colonial_centralization": true,
+  "tech_galactic_administration": true,
+  "tech_food_processing_1": true,
+  "tech_gene_crops": true,
+  "tech_colonization_1": true
+};
+
 @Component({
   selector: 'app-technology-web',
   templateUrl: './technology-web.component.html',
@@ -11,19 +19,24 @@ import * as _ from "lodash";
 export class TechnologyWebComponent implements OnInit {
   g: dagre.graphlib.Graph;
   nodes: dagre.Node[];
-  edges: dagre.GraphEdge[];
+  edges: dagre.Edge[];
 
   constructor(
     private store: TechnologyLibraryService
   ) { }
 
   ngOnInit() {
-    this.g = new dagre.graphlib.Graph();
+    this.g = new dagre.graphlib.Graph({
+      directed: true
+    });
     this.g.setGraph({
       marginx: 50,
       marginy: 50,
-      rankdir: 'RL',
-
+      rankdir: 'LR',
+      ranksep: 200,
+      nodesep: 35,
+      edgesep: 10,
+      compound: true,
     });
     this.g.setDefaultEdgeLabel(function() { return {}; });
 
@@ -36,8 +49,8 @@ export class TechnologyWebComponent implements OnInit {
     this.store.techs.forEach((t) => {
       if (t.prerequisites) {
         t.prerequisites.forEach((preReq) => {
-          if (preReq.id) {
-            this.g.setEdge(t.id, preReq.id);
+          if (preReq.id && (LOW_WEIGHT_TECHS[preReq.id] === undefined)) {
+            this.g.setEdge(preReq.id, t.id, { weight: 1 });
           }
         });
       }
@@ -46,7 +59,7 @@ export class TechnologyWebComponent implements OnInit {
     dagre.layout(this.g);
 
     this.nodes = this.g.nodes().map((v) => this.g.node(v));
-    this.edges = this.g.edges().map((e) => this.g.edge(e));
+    this.edges = this.g.edges();
   }
 
   layoutNode(n: dagre.Node) {
@@ -58,17 +71,24 @@ export class TechnologyWebComponent implements OnInit {
     }
   }
 
-  strokeEdge(e: dagre.GraphEdge) {
-    return _.reduce(
-      e.points
-      , (accumulator, point, index) => {
-        if (index === 0) {
-          return `M ${point.x} ${point.y} `;
-        } else {
-          return accumulator + `L ${point.x} ${point.y} `;
+  strokeEdge(e: dagre.Edge) {
+    if (false) {
+      let eDef = this.g.edge(e);
+      return _.reduce(
+        eDef.points
+        , (accumulator, point, index) => {
+          if (index === 0) {
+            return `M ${point.x} ${point.y} `;
+          } else {
+            return accumulator + `L ${point.x} ${point.y} `;
+          }
         }
-      }
-      , ''
-    );
+        , ''
+      );
+    } else {
+      let start = this.g.node(e.v);
+      let end = this.g.node(e.w);
+      return `M ${start.x + start.width/2} ${start.y} L ${end.x - end.width/2} ${end.y}`;
+    }
   }
 }
